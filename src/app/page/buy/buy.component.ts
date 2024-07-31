c2bba6bc4ec8618f8271aa994d4c6a4c3f718082
Amisha <amisha1324s@gmail.com>
2024-07-31 16:48:57 +0530

    buttonand container changes

--- src/app/page/buy/buy.component.ts
+++ src/app/page/buy/buy.component.ts
@@ -3,11 +3,12 @@ import { MenuComponent } from '../menu/menu.component';
 import { IonicModule } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule}  from '@angular/material/icon';
 @Component({
   selector: 'app-buy',
   standalone: true,
-  imports: [MenuComponent,IonicModule,CommonModule, FormsModule],
+  imports: [MenuComponent,IonicModule,CommonModule, FormsModule,MatButtonModule,MatIconModule],
   templateUrl: './buy.component.html',
   styleUrl: './buy.component.scss'
 })
--- src/app/page/menu/menu.component.ts
+++ src/app/page/menu/menu.component.ts
@@ -2,10 +2,12 @@
 import { Component } from '@angular/core';
 // import { MatIconModule } from '@angular/material/icon';
 import { Router } from '@angular/router';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule}  from '@angular/material/icon';
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule,MatIconModule],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.scss'
 })
